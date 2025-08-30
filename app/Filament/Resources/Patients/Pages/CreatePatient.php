7489bc0e4387798bfdfe2d88bb431ea4bb2b401a
ilhamizzul <ilhamizzul@gmail.com>
2025-08-30 22:10:25 +0700

    Add success notifications for patient creation and update actions

--- app/Filament/Resources/Patients/Pages/CreatePatient.php
+++ app/Filament/Resources/Patients/Pages/CreatePatient.php
@@ -3,9 +3,18 @@
 namespace App\Filament\Resources\Patients\Pages;
 
 use App\Filament\Resources\Patients\PatientResource;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreatePatient extends CreateRecord
 {
     protected static string $resource = PatientResource::class;
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->title('Patient created successfully')
+            ->body('The patient record has been created successfully.')
+            ->success();
+    }
 }
--- app/Filament/Resources/Patients/Pages/EditPatient.php
+++ app/Filament/Resources/Patients/Pages/EditPatient.php
@@ -4,11 +4,21 @@ namespace App\Filament\Resources\Patients\Pages;
 
 use App\Filament\Resources\Patients\PatientResource;
 use Filament\Actions\DeleteAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditPatient extends EditRecord
 {
     protected static string $resource = PatientResource::class;
+
+
+    protected function getUpdatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->title('Patient updated successfully')
+            ->body('The patient record has been updated successfully.')
+            ->success();
+    }
 
     protected function getHeaderActions(): array
     {
